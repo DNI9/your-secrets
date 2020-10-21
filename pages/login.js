c1d78d04a8d5e45c06cf5ddd82f3bda0d5c3d743
DNI9 <itsr9boi@gmail.com>
2020-10-22 00:38:45 +0530

    add login page

---
+++ pages/login.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import {useRouter} from 'next/router';
+
+const login = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log('well hello');
+    router.push('/');
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Login to Your secrets</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='login'>
+        <div className='login__button' onClick={handleClick}>
+          <img src='/google-icon.svg' alt='google icon svg' />
+          <h3>Login with Google</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
