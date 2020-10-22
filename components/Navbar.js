9a73e486842ce5abc5ad3ac4a56af3fcc75ee407
DNI9 <itsr9boi@gmail.com>
2020-10-22 21:41:13 +0530

    add avatar component to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,25 @@
 import {useAuth} from 'context/AuthContext';
+import {useRouter} from 'next/router';
+import Avatar from './Avatar';
 
 const Navbar = ({title, showUserAvatar}) => {
-  const {currentUser, loading} = useAuth();
+  const {currentUser, logout, loading} = useAuth();
+  const router = useRouter();
+
+  // TODO: handle logout for now, use short modal later
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+        router.push('/login');
+      })
+      .catch(err => console.error(err));
+  };
   return (
     <div className='navbar'>
       <div className='navbar__title'>{title || 'Secrets'}</div>
       {showUserAvatar && (
-        <div className='navbar__user-avatar'>
-          {loading && <img src='/user.svg' alt='user avatar icon' />}
-          {!loading && (
-            <img
-              src={currentUser.photoURL}
-              className='avatar-img'
-              alt='user avatar icon'
-            />
-          )}
+        <div className='navbar__user-avatar' onClick={handleLogout}>
+          <Avatar photoURL={currentUser?.photoURL} />
         </div>
       )}
     </div>
