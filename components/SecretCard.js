73098bde4cf1f12d15f4c013601d55eccc83a4f1
DNI9 <itsr9boi@gmail.com>
2020-10-22 17:52:28 +0530

    add onclick on card title on secret card

--- components/SecretCard.js
+++ components/SecretCard.js
@@ -1,10 +1,18 @@
+import {useRouter} from 'next/router';
+
 const SecretCard = ({name, noOfMessages}) => {
+  const router = useRouter();
+  const onCardClick = () => {
+    router.push('/secrets/1');
+    console.log('Card clicked');
+  };
+
   return (
     <div className='card'>
       <div className='card__icon'>
         <img src='/lock.svg' />
       </div>
-      <div className='card__title'>
+      <div className='card__title' onClick={onCardClick}>
         <h2>{name}</h2>
         <p>{noOfMessages} messages</p>
       </div>
