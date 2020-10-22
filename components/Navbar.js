89d39bb4419bf6a54c079f2e21a055f8581225af
DNI9 <itsr9boi@gmail.com>
2020-10-22 21:21:21 +0530

    show user photo when user loaded on nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,20 @@
+import {useAuth} from 'context/AuthContext';
+
 const Navbar = ({title, showUserAvatar}) => {
+  const {currentUser, loading} = useAuth();
   return (
     <div className='navbar'>
       <div className='navbar__title'>{title || 'Secrets'}</div>
       {showUserAvatar && (
         <div className='navbar__user-avatar'>
-          <img src='/user.svg' alt='user avatar icon' />
+          {loading && <img src='/user.svg' alt='user avatar icon' />}
+          {!loading && (
+            <img
+              src={currentUser.photoURL}
+              className='avatar-img'
+              alt='user avatar icon'
+            />
+          )}
         </div>
       )}
     </div>
