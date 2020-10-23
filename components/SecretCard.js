928b46ef404b87e22bf921a89055637ce7aa84e5
DNI9 <itsr9boi@gmail.com>
2020-10-23 21:59:26 +0530

    hotfix: use next link instead of route

--- components/SecretCard.js
+++ components/SecretCard.js
@@ -1,21 +1,23 @@
-import {useRouter} from 'next/router';
+// import {useRouter} from 'next/router';
 import {motion} from 'framer-motion';
+import Link from 'next/link';
 
 const SecretCard = ({name, noOfMessages, id}) => {
-  const router = useRouter();
-  const onCardClick = () => {
-    router.push('/secrets/[secretID]', `/secrets/${id}`);
-  };
-
+  // const router = useRouter();
+  // const onCardClick = () => {
+  //   router.push('/secrets/[secretID]', `/secrets/${id}`);
+  // };
   return (
     <motion.div layout transition={{duration: 0.5}} className='card'>
       <div className='card__icon'>
         <img src='/lock.svg' />
       </div>
-      <div className='card__title' onClick={onCardClick}>
-        <h2>{name}</h2>
-        <p>{noOfMessages} messages</p>
-      </div>
+      <Link href='/secrets/[secretID]' as={`/secrets/${id}`}>
+        <div className='card__title'>
+          <h2>{name}</h2>
+          <p>{noOfMessages} messages</p>
+        </div>
+      </Link>
       <div className='card__right-icon'>
         <img src='share.svg' />
       </div>
