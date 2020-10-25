27cc91cc24615e81d520fe8a7923dddb4b1005d1
DNI9 <itsr9boi@gmail.com>
2020-10-25 14:54:37 +0530

    delete messages belonging to a secret
    after deleting the secret

--- utils/deleteDoc.js
+++ utils/deleteDoc.js
@@ -1,3 +1,22 @@
 import {db} from 'config/firebase';
 
-export const deleteSecret = async id => await db.doc(`secrets/${id}`).delete();
+export const deleteSecret = async id => {
+  async function deleteMsgsBelongsToSecret() {
+    const querySnapshot = await db
+      .collection('messages')
+      .where('secretID', '==', id)
+      .get();
+
+    const batch = db.batch();
+    querySnapshot.forEach(doc => {
+      batch.delete(doc.ref);
+    });
+    return batch.commit();
+  }
+  try {
+    await db.doc(`secrets/${id}`).delete();
+    await deleteMsgsBelongsToSecret();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
