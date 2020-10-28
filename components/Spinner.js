21f5fb0fee0c8a131efd7dbe396ef0e640ab0603
DNI9 <itsr9boi@gmail.com>
2020-10-28 18:13:11 +0530

    use Image component from nextjs for spinner

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+
 const Spinner = () => (
   <div className='spinner'>
-    <img src='/loading_doge.gif' />
+	<Image
+        src="/loading_doge.gif"
+        alt="Loading doge"
+        width={80}
+        height={80}
+      />
   </div>
 );
 
+//TODO: use next/image
 export default Spinner;
