85e326574c58097b67661ef5fcff1cb6f8162062
DNI9 <itsr9boi@gmail.com>
2020-10-28 18:20:07 +0530

    revert back to normal img element from Image component

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,15 +1,7 @@
-import Image from 'next/image';
-
 const Spinner = () => (
   <div className='spinner'>
-	<Image
-        src="/loading_doge.gif"
-        alt="Loading doge"
-        width={80}
-        height={80}
-      />
+	<img src='/loading_doge.gif' />
   </div>
 );
 
-//TODO: use next/image
 export default Spinner;
